750e5318f034d24023fda6e7d21266370d4d1dea
Neeiser <stortini.study@gmail.com>
2022-05-04 15:38:01 +0200

    Linked everything to display a lorem - Route + Model + Controller function + View lorem

---
+++ resources/views/MovieView.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel-model-controller</title>
+</head>
+<body>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut quibusdam, nobis delectus culpa, reiciendis illum porro ipsa odit laboriosam debitis cupiditate dignissimos repellat cumque quas eaque non perspiciatis fugit cum.
+</body>
+</html>
