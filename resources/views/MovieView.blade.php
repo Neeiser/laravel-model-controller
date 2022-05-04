9ecfb4cd81294fd78d31d189e33c75b48a2a5a78
Neeiser <stortini.study@gmail.com>
2022-05-04 16:30:10 +0200

    Exercise done

--- resources/views/MovieView.blade.php
+++ resources/views/MovieView.blade.php
@@ -5,10 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>laravel-model-controller</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    @foreach ($movies as $movieItem)
-        <p>{{$movieItem->title}}</p>
-    @endforeach
+    <main>
+        <div class="movie-container">
+            @foreach ($movies as $movieItem)
+                <div class="movie-card">
+                    <p>{{$movieItem->title}}</p>
+                    <p>{{$movieItem->original_title}}</p>
+                    <p>{{$movieItem->nationality}}</p>
+                    <p>{{$movieItem->date}}</p>
+                    <p>{{$movieItem->vote}}</p>
+                </div>
+            @endforeach
+        </div>
+    </main>
 </body>
 </html>
